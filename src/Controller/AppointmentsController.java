a883ae546452ef8eb20e50f8c532d9f7d394a22d
JoshHerde <jherde2@wgu.edu>
2022-06-23 18:18:35 -0400

    add

--- src/Controller/AppointmentsController.java
+++ src/Controller/AppointmentsController.java
@@ -1,45 +1,73 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class AppointmentsController {
-    public TableView appointmentsTable;
-    public TableColumn idCol;
-    public TableColumn titleCol;
-    public TableColumn descCol;
-    public TableColumn locCol;
-    public TableColumn contactCol;
-    public TableColumn typeCol;
-    public TableColumn startCol;
-    public TableColumn endCol;
-    public TableColumn customerIdCol;
-    public TableColumn userIdCol;
-    public RadioButton monthRadioButton;
-    public RadioButton weekRadioButton;
-    public TextField searchTextField;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public void monthRbClicked(ActionEvent actionEvent) {
+public class AppointmentsController implements Initializable {
+
+    @FXML private TableView appointmentsTable;
+    @FXML private TableColumn idCol;
+    @FXML private TableColumn titleCol;
+    @FXML private TableColumn descCol;
+    @FXML private TableColumn locCol;
+    @FXML private TableColumn contactCol;
+    @FXML private TableColumn typeCol;
+    @FXML private TableColumn startCol;
+    @FXML private TableColumn endCol;
+    @FXML private TableColumn customerIdCol;
+    @FXML private TableColumn userIdCol;
+    @FXML private  RadioButton monthRadioButton;
+    @FXML private  RadioButton weekRadioButton;
+    @FXML private  TextField searchTextField;
+
+    @FXML void monthRbClicked(ActionEvent actionEvent) {
     }
 
-    public void weekRbClicked(ActionEvent actionEvent) {
+    @FXML void weekRbClicked(ActionEvent actionEvent) {
     }
 
-    public void newAppointmentClicked(ActionEvent actionEvent) {
+    @FXML void newAppointmentClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/AddAppointment.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void modifyAppointmentClicked(ActionEvent actionEvent) {
+    @FXML void modifyAppointmentClicked(ActionEvent actionEvent) {
     }
 
-    public void deleteAppointmentClicked(ActionEvent actionEvent) {
+    @FXML void deleteAppointmentClicked(ActionEvent actionEvent) {
     }
 
-    public void exitButtonClicked(ActionEvent actionEvent) {
+    @FXML void exitButtonClicked(ActionEvent actionEvent) {
+        System.exit(0);
     }
 
-    public void customerButtonClicked(ActionEvent actionEvent) {
+    @FXML void customerButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/Customers.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
--- src/Controller/CustomersController.java
+++ src/Controller/CustomersController.java
@@ -2,9 +2,16 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CustomersController {
 
@@ -26,6 +33,11 @@ public class CustomersController {
     @FXML void deleteCustomerClicked(ActionEvent actionEvent) {
     }
 
-    @FXML void backButtonClicked(ActionEvent actionEvent) {
+    @FXML void backButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/View/Appointments.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
